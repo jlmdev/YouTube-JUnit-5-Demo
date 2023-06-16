a52fd852c1396435ae07d4874b8f68a2e4a4fd10
Josh Mann <josh@jlmann.com>
2023-06-16 11:07:15 -0400

    And Again...

---
+++ YouTube-JUnit-5-Demo/src/main/java/jlmdev/YouTube_JUnit_5_Demo/App.java
@@ -0,0 +1,13 @@
+package jlmdev.YouTube_JUnit_5_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
